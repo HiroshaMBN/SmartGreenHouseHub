ffc785031b47e19c0c69325bae15e1377fd8f9b5
hirohab <hiroshab@bluechip.lk>
2025-02-11 09:04:56 +0530

    all user management

--- app/Http/Controllers/UserManageControllers/UserManageController.php
+++ app/Http/Controllers/UserManageControllers/UserManageController.php
@@ -9,5 +9,24 @@ use Illuminate\Http\Request;
 
 class UserManageController extends Controller
 {
+    public function updateUserProfile(Request $request){
 
+      try{
+
+        if(!$request->user() == "NULL"){
+            return response()->json(["message"=>"Empty User"]);
+            exit();
+        }
+
+        if($request->user()->tokenCan('read-profile')){
+            return response()->json(["message"=>"User Profile Updated Successfully"],200);
+
+            }else{
+                abort(403, 'Unauthorized');
+            }
+      }catch(Exception $exception){
+        return response()->json(["message"=>$exception->getMessage()],406);
+      }
+
+    }
 }
