4ef61beaebc8eb119ff049aa4425c72caa9fd192
root <HiroshaMBN>
2025-04-24 13:30:49 -0400

    add sheduler for run the function for sent the notification timely

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,9 +15,33 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $notificationTime = env('NOTIFICATION_TIME', 'everyMinute'); // Default to every minute if not set
 
-        $schedule->command('queue:work --queue=sensor/dht11 --tries=3')->everyMinute();
+    // Dynamic scheduling based on the value of NOTIFICATION_TIME in the .env file
+    switch ($notificationTime) {
+        case 'everyMinute':
+            $schedule->command('send:mq2Notify')->everyMinute();
+            $schedule->command('send:humidNotify')->everyMinute();
+            $schedule->command('send:soilNotify')->everyMinute();
+            $schedule->command('send:temNotify')->everyMinute();
+            break;
+
+        case 'everyFiveMinutes':
+            $schedule->command('send:mq2Notify')->everyFiveMinutes();
+            $schedule->command('send:humidNotify')->everyFiveMinutes();
+            $schedule->command('send:soilNotify')->everyFiveMinutes();
+            $schedule->command('send:temNotify')->everyFiveMinutes();
+            break;
+
+        case 'hourly':
+            $schedule->command('send:mq2Notify')->hourly();
+            $schedule->command('send:humidNotify')->hourly();
+            $schedule->command('send:soilNotify')->hourly();
+            $schedule->command('send:temNotify')->hourly();
+            break;
+
+        // You can add other cases here as needed...
+    }
     }
 
     /**
